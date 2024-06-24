caac977cca5a503c92bd1392dd046c7cdddf8783
Ansh Kumar Singh <148905627+AnshKumar1936@users.noreply.github.com>
2024-06-24 23:46:27 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+echo "helloworld<br>";
+
+$name = "Ansh Kumar Singh";
+$age = 20;
+$rollno = 84;
+
+echo "My name is : " .$name;
+echo "<br>My age is : $age";
+echo "<br>My rollno is : {$rollno}";
+echo "<br>";
+echo "<br>";
+var_dump($name);
+echo "<br>";
+var_dump($age);
+echo "<br>";
+var_dump($rollno);
